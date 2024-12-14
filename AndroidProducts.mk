44b4a00c69d4c7dbc4345f3585237a981ec7f061
ZetLink <royalezetlink@gmail.com>
2024-12-14 16:33:41 +0100

    Init Afterlife

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,4 +4,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rhode.mk
+    $(LOCAL_DIR)/afterlife_rhode.mk
--- afterlife_rhode.mk
+++ afterlife_rhode.mk
@@ -10,16 +10,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device
 $(call inherit-product, device/motorola/rhode/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Afterlife stuff.
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_rhode
+PRODUCT_NAME := afterlife_rhode
 PRODUCT_DEVICE := rhode
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g52
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+TARGET_USE_ECHO := true
+TARGET_SUPPORTS_BLUR := false
+TARGET_UDFPS_ANIMATIONS := false
+TARGET_BOOT_ANIMATION_RES := 1080
 
 BUILD_FINGERPRINT := "motorola/rhode_g/rhode:13/T2SRS33.72-22-4-8/07e10-4fa4c:user/release-keys"
 
